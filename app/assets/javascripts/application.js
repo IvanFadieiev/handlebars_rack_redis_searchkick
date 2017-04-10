0ec3408c522ec3ebb3a448822c32fb1eafed0add
IvanFadieiev <ivanfadeev91@gmail.com>
2017-04-10 11:12:57 +0300

    Add for $(".class").ready selector "jquery.readyselector"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //= require handlebars.runtime
 //= require_tree ./templates
 //= require users
+//= require jquery.readyselector
 //= require handlebars
 
 function new_post(post){
@@ -10,20 +11,20 @@ function new_post(post){
     $('.AZAZA').prepend(data);
 };
 
-function load_to_show_page(id){
-    $(function(){
-        $.ajax({
-            url: '/user_posts',
-            data: {id: id},
-            success: function(data){
-                posts_list({posts: data});
-            },
-            error: function(){
-                alert('Error(((')
-            }
-        })
-    })
-};
+// function load_to_show_page(id){
+//     $(function(){
+//         $.ajax({
+//             url: '/user_posts',
+//             data: {id: id},
+//             success: function(data){
+//                 posts_list({posts: data});
+//             },
+//             error: function(){
+//                 alert('Error(((')
+//             }
+//         })
+//     })
+// };
 
 function posts_list(parameters){
     var posts = parameters.posts;
@@ -31,3 +32,21 @@ function posts_list(parameters){
     $('.posts_list').html(data);
 };
 
+
+$('.users.show').ready(function (){
+
+    var path_name = window.location.pathname;
+    var path_name_array = path_name.split('/');
+    var id = path_name_array[path_name_array.length - 1];
+    
+    $.ajax({
+        url: '/user_posts',
+        data: {id: id},
+        success: function(data){
+            posts_list({posts: data});
+        },
+        error: function(){
+            alert('Error(((')
+        }
+    });
+});
